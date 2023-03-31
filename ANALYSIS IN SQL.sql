2916adfdf03afaa1451deab0498270fa0261b02c
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-03-31 19:44:34 +0530

    Update ANALYSIS IN SQL.sql

--- ANALYSIS IN SQL.sql
+++ ANALYSIS IN SQL.sql
@@ -30,3 +30,11 @@
 
     `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.currency="INR\r" or transactions.currency="USD\r";`
 	
+-- QUE8. Show total revenue in year 2020, January Month,
+
+    `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and and date.month_name="January" and (transactions.currency="INR\r" or transactions.currency="USD\r");`
+
+-- QUE9. Show total revenue in year 2020 in Chennai
+
+    `SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020
+and transactions.market_code="Mark001";`
